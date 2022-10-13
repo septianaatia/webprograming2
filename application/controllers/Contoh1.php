6fe3c6f5c5914b5439f7fbd68e6df59a5b15cd4e
septianaatia <septianatia099@gmail.com>
2022-10-13 20:27:08 +0700

    membuar form validasi

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Tia Septiana
+            Saya tinggal di daerah Pesing
+            olahraga yang saya sukai adalah 
+            lari";
+	}
+}
